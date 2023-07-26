124382313c3c3d8797ab1e5db9d0166565236023
sergiooak <sergiopcn@gmail.com>
2023-07-26 20:04:31 -0300

    docs: add types

--- src/config/reactions.js
+++ src/config/reactions.js
@@ -1,3 +1,8 @@
+/**
+ * Reactions that the bot will use, based on the situation
+ * @type {Object.<string, Emoji>}
+ * @typedef {string} Emoji - The emoji, eg: '✅'
+ */
 export default {
   wait: '⏳',
   success: '✅',
