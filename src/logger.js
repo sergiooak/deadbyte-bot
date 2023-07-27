2d61fd9090d3af8298714f9b191d807b840c34cc
sergiooak <sergiopcn@gmail.com>
2023-07-27 00:32:37 -0300

    feat: save error logs to file

--- src/logger.js
+++ src/logger.js
@@ -1,10 +1,29 @@
 import pino from 'pino'
 
+const transport = pino.transport({
+  targets: [
+    {
+      level: 'warn',
+      target: 'pino/file',
+      options: {
+        destination: 'errors.log'
+      }
+    },
+    {
+      level: 'trace',
+      target: 'pino-pretty',
+      options: {}
+    }
+  ]
+})
+
+/**
+ * @type {import('pino').Logger}
+ * @see https://getpino.io/#/
+ */
 export default pino(
   {
-    level: 'info',
-    transport: {
-      target: 'pino-pretty'
-    }
-  }
+    level: 'info'
+  },
+  transport
 )
