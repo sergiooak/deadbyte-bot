d76ff229d6b666f0e3ea35b3ca704eb3d78acc7a
sergiooak <sergiopcn@gmail.com>
2023-07-29 01:07:03 -0300

    refactor: functions/sticker.js

--- src/services/functions/sticker.js
+++ src/services/functions/sticker.js
@@ -18,37 +18,13 @@ import FormData from 'form-data'
 export async function sticker (msg, crop = false, stickerAuthor, stickerPack) {
   await msg.react(reactions.wait)
 
-  let media = await msg.downloadMedia()
-  if (!media) return logger.error('Error downloading media')
+  const media = await msg.downloadMedia()
+  if (!media) throw new Error('Error downloading media')
+
   let stickerMedia = await Util.formatToWebpSticker(media, {}, crop)
-  let mediaBuffer = Buffer.from(stickerMedia.data, 'base64')
+  if (msg.body) stickerMedia = await overlaySubtitle(msg.body, stickerMedia).catch((e) => logger.error(e)) || stickerMedia
 
-  // if message has body, add it to the sticker as subtitle
-  if (msg.body) stickerMedia = await overlaySubtitle(msg.body, mediaBuffer).catch((e) => logger.error(e)) || stickerMedia
-
-  const chat = await msg.getChat()
-
-  media = new wwebjs.MessageMedia('image/webp', stickerMedia.data, 'deadbyte.webp', true)
-  mediaBuffer = Buffer.from(stickerMedia.data, 'base64')
-
-  // if heavier than 1MB, compress it
-  if (mediaBuffer.byteLength > 1_000_000) {
-    logger.debug('compressing sticker...', mediaBuffer.byteLength)
-    const compressedBuffer = await sharp(mediaBuffer, { animated: true })
-      .webp({ quality: 33 })
-      .toBuffer()
-    media = new wwebjs.MessageMedia('image/webp', compressedBuffer.toString('base64'), 'deadbyte.webp', true)
-    logger.debug('compressed sticker!', mediaBuffer.byteLength, '->', compressedBuffer.byteLength)
-    mediaBuffer = Buffer.from(media.data, 'base64')
-  }
-
-  // if still heavier than 1MB, throw error
-  if (mediaBuffer.byteLength > 1_000_000) {
-    logger.warn('sticker is still too heavy!', mediaBuffer.byteLength)
-    return await msg.react(reactions.heavy)
-  }
-
-  await sendMediaAsSticker(chat, media, stickerAuthor, stickerPack)
+  await sendMediaAsSticker(msg.aux.chat, stickerMedia, stickerAuthor, stickerPack)
 
   if (!crop) {
     await sticker(msg, true) // make cropped version
@@ -63,23 +39,12 @@ export async function sticker (msg, crop = false, stickerAuthor, stickerPack) {
 export async function stickerText (msg) {
   await msg.react(reactions.wait)
 
-  const url = await createUrl('image-creator', 'ttp', {
-    message: msg.body
-  })
+  const url = await createUrl('image-creator', 'ttp', { message: msg.body })
 
-  const media = await wwebjs.MessageMedia.fromUrl(url, {
-    unsafeMime: true
-  })
-  if (!media) {
-    logger.error('Error downloading media')
-    return await msg.react(reactions.error)
-  }
+  const media = await wwebjs.MessageMedia.fromUrl(url, { unsafeMime: true })
+  if (!media) throw new Error('Error downloading media')
 
-  const chat = await msg.getChat()
-
-  // send media as sticker back
-  await sendMediaAsSticker(chat, media)
-
+  await sendMediaAsSticker(msg.aux.chat, media)
   await msg.react(reactions.success)
 }
 
@@ -104,52 +69,20 @@ export async function removeBg (msg) {
   }
 
   const media = await msg.downloadMedia()
-  if (!media) return logger.error('Error downloading media')
+  if (!media) throw new Error('Error downloading media')
   if (!media.mimetype.includes('image')) {
     await msg.react(reactions.error)
     return await msg.reply('❌ Só consigo remover o fundo de imagens')
   }
 
-  // upload image to get temporary url
-  const formData = new FormData()
-  const buffer = Buffer.from(media.data, 'base64')
-  formData.append('file', buffer, 'image.png')
-
-  const response = await fetch('https://v1.deadbyte.com.br/uploader/tempurl', {
-    method: 'POST',
-    body: formData
-  })
-
-  if (!response.ok) {
-    logger.error('Error uploading image to remove.bg')
-    return await msg.react(reactions.error)
-  }
-
-  const json = await response.json()
-  const tempUrl = json.result
-
-  // download image from temporary url
-  const image = await fetch(tempUrl)
-  if (!image.ok) {
-    logger.error('Error downloading image from remove.bg')
-    return await msg.react(reactions.error)
-  }
-
-  // send image as sticker back
-  const chat = await msg.getChat()
-
+  const tempUrl = await getTempUrl(media)
   const url = 'https://v1.deadbyte.com.br/image-processing/removebg?img=' + tempUrl + '&trim=true'
-  const bgMedia = await wwebjs.MessageMedia.fromUrl(url + tempUrl + '&trim=true', {
-    unsafeMime: true
-  })
+  const bgMedia = await wwebjs.MessageMedia.fromUrl(url + tempUrl + '&trim=true', { unsafeMime: true })
 
   let stickerMedia = await Util.formatToWebpSticker(bgMedia, {})
-  const mediaBuffer = Buffer.from(stickerMedia.data, 'base64')
+  if (msg.body) stickerMedia = await overlaySubtitle(msg.body, stickerMedia).catch((e) => logger.error(e)) || stickerMedia
 
-  // if message has body, add it to the sticker as subtitle
-  if (msg.body) stickerMedia = await overlaySubtitle(msg.body, mediaBuffer).catch((e) => logger.error(e)) || stickerMedia
-
-  await sendMediaAsSticker(chat, stickerMedia)
+  await sendMediaAsSticker(msg.aux.chat, stickerMedia)
   await msg.react(reactions.success)
 }
 
@@ -185,7 +118,7 @@ export async function stealSticker (msg) {
   }
 
   const media = await quotedMsg.downloadMedia()
-  if (!media) return logger.error('Error downloading media')
+  if (!media) throw new Error('Error downloading media')
 
   await sendMediaAsSticker(msg.aux.chat, media, stickerName, stickerAuthor)
 }
@@ -205,12 +138,25 @@ export async function stealSticker (msg) {
  * @returns {Promise<import ('whatsapp-web.js').Message>} A Promise that resolves with the Message object of the sent sticker.
  */
 async function sendMediaAsSticker (chat, media, stickerName, stickerAuthor) {
-  return await chat.sendMessage(media, {
-    sendMediaAsSticker: true,
-    stickerName: stickerName || 'DeadByte.com.br',
-    stickerAuthor: stickerAuthor || 'bot de figurinhas',
-    stickerCategories: ['💀', '🤖']
-  })
+  const buffer = Buffer.from(media.data, 'base64')
+
+  // if heavier than 1MB, compress it
+  if (buffer.byteLength > 1_000_000) {
+    media = await compressMediaBuffer(buffer)
+  }
+
+  media = new wwebjs.MessageMedia(media.mimetype || 'image/webp', media.data, media.filename || 'sticker.webp')
+
+  try {
+    return await chat.sendMessage(media, {
+      sendMediaAsSticker: true,
+      stickerName: stickerName || 'DeadByte.com.br',
+      stickerAuthor: stickerAuthor || 'bot de figurinhas',
+      stickerCategories: ['💀', '🤖']
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 /**
@@ -218,11 +164,13 @@ async function sendMediaAsSticker (chat, media, stickerName, stickerAuthor) {
  * @async
  * @function overlaySubtitle
  * @param {string} text - The subtitle text to overlay.
- * @param {Buffer} mediaBuffer - The media buffer to overlay the subtitle on.
+ * @param {import ('whatsapp-web.js').MessageMedia} stickerMedia - The media to overlay the subtitle on.
  * @returns {Promise<import ('whatsapp-web.js').MessageMedia>} A Promise that resolves with a new MessageMedia object containing the media with the subtitle overlayed.
  * @throws {Error} If there was an error downloading the subtitle media.
  */
-async function overlaySubtitle (text, mediaBuffer) {
+async function overlaySubtitle (text, stickerMedia) {
+  const mediaBuffer = Buffer.from(stickerMedia.data, 'base64')
+
   const url = await createUrl('image-creator', 'ttp', {
     message: text,
     subtitle: true
@@ -245,4 +193,49 @@ async function overlaySubtitle (text, mediaBuffer) {
 
   // replace media data with the new data from sharp
   return new wwebjs.MessageMedia('image/webp', finalBuffer.toString('base64'), 'deadbyte.webp', true)
+}
+
+/**
+ * Compresses a media buffer for a sticker image.
+ * @async
+ * @function compressMediaBuffer
+ * @param {Buffer} mediaBuffer - The media buffer to compress.
+ * @returns {Promise<wwebjs.MessageMedia>} A Promise that resolves with a compressed MessageMedia object.
+ * @throws {Error} If the compressed buffer is still too heavy.
+ */
+async function compressMediaBuffer (mediaBuffer) {
+  logger.debug('compressing sticker...', mediaBuffer.byteLength)
+  const compressedBuffer = await sharp(mediaBuffer, { animated: true })
+    .webp({ quality: 33 })
+    .toBuffer()
+  logger.debug('compressed sticker!', mediaBuffer.byteLength, '->', compressedBuffer.byteLength)
+
+  if (compressedBuffer.byteLength > 1_000_000) throw new Error('Sticker is still too heavy!', mediaBuffer.byteLength)
+
+  return new wwebjs.MessageMedia('image/webp', compressedBuffer.toString('base64'), 'deadbyte.webp', true)
+}
+
+/**
+ * Uploads an image to get a temporary URL
+ * @param {import ('whatsapp-web.js').MessageMedia} media - The media to upload
+ * @returns {promise<string>} A Promise that resolves with the temporary URL of the uploaded image.
+ */
+async function getTempUrl (media) {
+  const formData = new FormData()
+  formData.append('file', Buffer.from(media.data, 'base64'), media.filename || 'sticker.png')
+
+  const response = await fetch('https://v1.deadbyte.com.br/uploader/tempurl', {
+    method: 'POST',
+    body: formData
+  })
+
+  if (!response.ok) {
+    logger.error('Error uploading image to remove.bg')
+    throw new Error('Error uploading image to remove.bg')
+  }
+
+  const json = await response.json()
+  const tempUrl = json.result
+
+  return tempUrl
 }
