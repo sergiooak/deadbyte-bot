41976eebd16741b74fe59f9fc562fea1ffc86b17
sergiooak <sergiopcn@gmail.com>
2023-07-29 18:12:41 -0300

    refactor: import configs

--- src/config/bot.js
+++ src/config/bot.js
@@ -11,3 +11,13 @@ export const prefixes = ['!', '\\.', '#', '/']
  * @see "#noCommand" will be treated as a commandless message if no matching command is found
  */
 export const prefixesWithFallback = ['#']
+
+export const name = process.env.BOT_NAME || 'DeadByte'
+export const chromePath = process.env.CHROME_PATH || 'C:/Program Files/Google/Chrome/Application/chrome.exe'
+
+export default {
+  prefixes,
+  prefixesWithFallback,
+  name,
+  chromePath
+}
