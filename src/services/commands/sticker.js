e43b569343923226f9c55f5db2c9002c17bde6c5
sergiooak <sergiopcn@gmail.com>
2023-07-28 23:04:14 -0300

    fix: recieve aux object

--- src/services/commands/sticker.js
+++ src/services/commands/sticker.js
@@ -1,11 +1,10 @@
 /**
  * Sticker creation, modification etc
  * @param {import('whatsapp-web.js').Message} msg
- * @param {import('whatsapp-web.js').Chat} chat
- * @param {import('whatsapp-web.js').Client} client
+ * @param {object} aux
  * @returns {Object}
  */
-export default (msg, chat, client) => {
+export default (msg, aux) => {
   return {
     sticker: /^(s|sticker|f|fig)$/.test(aux.function) && isMediaStickerCompatible(msg),
     removeBg: /^(bg|fundo|nobg)$/.test(aux.function),
