691fd0370c32781d3b20b2dabc28bf97129b78fa
sergiooak <sergiopcn@gmail.com>
2023-07-31 13:06:22 -0300

    feta: add stickerly function

--- src/services/commands/sticker.js
+++ src/services/commands/sticker.js
@@ -9,6 +9,7 @@ export default (msg, aux) => {
     sticker: /^(s|sticker|f|fig)$/.test(aux.function),
     removeBg: /^(bg|fundo|nobg)$/.test(aux.function),
     stickerText: /^(ttp|ttp1|s|sticker|f|fig)$/.test(aux.function) && msg.type === 'chat',
-    stealSticker: /^(steal|roubar)$/.test(aux.function)
+    stealSticker: /^(steal|roubar)$/.test(aux.function),
+    stickerly: /^(stickerly|ly|lu)$/.test(aux.function)
   }
 }
