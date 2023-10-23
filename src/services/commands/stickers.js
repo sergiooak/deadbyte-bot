611ec94efb8f8c67a3c58f5e83ded77f1cccef5e
sergiooak <sergiopcn@gmail.com>
2023-10-22 22:53:41 -0300

    aaaa

--- src/services/commands/stickers.js
+++ src/services/commands/stickers.js
@@ -9,7 +9,7 @@ export default (msg, aux) => {
     'sticker-creator': /^(s|sticker|f|fig)$/.test(aux.function),
     'remove-bg': /^(bg|fundo|nobg)$/.test(aux.function),
     'text-sticker': /^(ttp|ttp1|s|sticker|f|fig)$/.test(aux.function) && msg.type === 'chat',
-    'text-sticker-2': /^(ttp2)$/.test(aux.function) && msg.type === 'chat',
+    'text-sticker2': /^(ttp2)$/.test(aux.function) && msg.type === 'chat',
     'steal-sticker': /^(steal|roubar)$/.test(aux.function),
     'sticker-ly-search': /^(stickerly|ly|lu)\d*$/.test(aux.function)
   }
