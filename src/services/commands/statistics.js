fbb62dffdfa7d16237b31d8de8828132358b1dc1
sergiooak <sergiopcn@gmail.com>
2023-11-11 14:51:04 -0300

    feat(statistics): add dayStats command

--- src/services/commands/statistics.js
+++ src/services/commands/statistics.js
@@ -9,6 +9,7 @@ export default (msg, aux) => {
     stats: /^(stats|estatisticas|estatistica|statistics|stat)$/.test(aux.function),
     botStats: /^(bstats|botstats|estatisticasbot|botstatistics|bstat)$/.test(aux.function),
     weekStats: /^(wstats|weekstats|estatisticassemana|weekstatistics|wstat)$/.test(aux.function),
+    dayStats: /^(dstats|daystats|estatisticasdia|daystatistics|dstat)$/.test(aux.function),
     hourStats: /^(hstats|hourstats|estatisticashora|hourstatistics|hstat)$/.test(aux.function)
   }
 }
