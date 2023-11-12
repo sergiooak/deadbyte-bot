2e99774a71d7d46079a0b7b45d9904372fa9e2cb
sergiooak <sergiopcn@gmail.com>
2023-11-12 18:20:44 -0300

    feat(stickers): add background removal options

--- src/services/commands/stickers.js
+++ src/services/commands/stickers.js
@@ -6,7 +6,7 @@
 export default (msg) => {
   return {
     'sticker-creator': /^(s|sticker|f|fig)$/.test(msg.aux.function),
-    'remove-bg': /^(bg|fundo|nobg)$/.test(msg.aux.function),
+    'remove-bg': /^(bg|fundo|nobg|semfundo|nobg|sfundo|png)$/.test(msg.aux.function),
     'text-sticker': /^(ttp|ttp1|s|sticker|f|fig)$/.test(msg.aux.function) && msg.type === 'chat',
     'text-sticker2': /^(ttp2)$/.test(msg.aux.function) && msg.type === 'chat',
     'text-sticker3': /^(ttp3)$/.test(msg.aux.function) && msg.type === 'chat',
