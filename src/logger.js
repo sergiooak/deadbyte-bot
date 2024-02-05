37de50eb6a59324a07ef45e820db7312daa206f0
sergiooak <sergiopcn@gmail.com>
2024-02-04 23:06:39 -0300

    chore: simplify logger

--- src/logger.js
+++ src/logger.js
@@ -1,29 +1,14 @@
 import pino from 'pino'
 
-const transport = pino.transport({
-  targets: [
-    {
-      level: 'warn',
-      target: 'pino/file',
-      options: {
-        destination: 'errors.log'
-      }
-    },
-    {
-      level: 'trace',
-      target: 'pino-pretty',
-      options: {}
-    }
-  ]
-})
-
 /**
  * @type {import('pino').Logger}
  * @see https://getpino.io/#/
  */
 export default pino(
   {
-    level: 'info'
-  },
-  transport
+    level: 'info',
+    transport: {
+      target: 'pino-pretty'
+    }
+  }
 )
