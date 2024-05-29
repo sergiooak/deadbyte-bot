df2e37d55debabf7a58ee96d791b02dbe3ad2a7f
Sergio Carvalho <sergiopcn@gmail.com>
2024-05-29 19:03:28 -0300

    refactor: Remove unused code for getting all chats

--- src/services/functions/admin-commands.js
+++ src/services/functions/admin-commands.js
@@ -1,5 +1,4 @@
 import relativeTime from 'dayjs/plugin/relativeTime.js'
-import { getLags } from '../../utils/lagMemory.js'
 import { getClient } from '../../index.js'
 import 'dayjs/locale/pt-br.js'
 import dayjs from 'dayjs'
@@ -20,10 +19,4 @@ export async function debug (msg) {
   await msg.react(debugEmoji)
 
   // debug code here
-
-  // get all chats
-  const client = getClient()
-  const chats = await client.getChats()
-  const groups = chats.filter(chat => chat.isGroup)
-  await msg.reply(groups.map(group => `${group.name} - ${group.id._serialized}`).join('\n\n'))
 }
