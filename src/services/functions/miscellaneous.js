659db5f2d4c564741d86d6a9dfc407bcec83c1ac
Sergio Carvalho <sergiopcn@gmail.com>
2024-07-02 16:45:13 -0300

    fix(TS): use the rith temp folder

--- src/services/functions/miscellaneous.js
+++ src/services/functions/miscellaneous.js
@@ -290,7 +290,8 @@ export async function transcribe (msg) {
 
   // save file to temp folder
   const timestampish = Date.now().toString().slice(-10)
-  const filePath = `./temp/${timestampish}.mp3`
+  console.log(timestampish)
+  const filePath = `./src/temp/${timestampish}.mp3`
   const nomalizedFilePath = path.resolve(filePath)
   fs.writeFileSync(nomalizedFilePath, media.data, { encoding: 'base64' })
 
