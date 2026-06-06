e0c258ec5db993f5950f88e26cfd2a35687e5791
Sergio Carvalho <sergiopcn@gmail.com>
2026-06-06 16:30:05 -0300

    feat(sticker.messages): enhance sticker messages with additional expressions
    - Improve media download failure message with varied expressions.
    - Update missing media request message for better clarity and tone.
    - Revise creation failure message to include more expressive options.
    - Enhance steal missing media message for clearer instructions.
    - Modify to media missing message for improved user guidance.
    - Refine conversion failed message with additional expressions.

--- src/messages/sticker.messages.ts
+++ src/messages/sticker.messages.ts
@@ -1,15 +1,15 @@
 export const stickerMessages = {
   mediaDownloadFailed:
-    '{{Putz|Vixe|Eita}, deu ruim|Deu certo não|Não rolou} de baixar essa mídia. {Tenta mandar de novo daqui a pouco|{Manda|Envia|Tenta} aí {novamente|de novo} que eu finjo que foi {culpa da|a} internet{| kk}|{Manda|Tenta} de novo, vai que dessa vez da certo{| uai| kk}}.',
+    '{Putz|Vixe|Eita porra}, {deu ruim ao|não rolou de|deu certo não,|deu {merda|xablau} ao}{| tentar} {baixar {essa mídia|esse arquivo}|fazer download {dessa mídia|desse arquivo}}. {{Tenta mandar|Manda} de novo{| daqui a pouco}}{|que eu finjo que foi {culpa da|a} {internet|net}{| kk}|ou tenta com {outro arquivo|outra mídia}{| uai| kk}}{|...|!| 🫠| 😁| 😇}',
 
   missingCreationMedia:
-    '{Manda|Envia|Me manda|Me envia} uma {foto|imagem}, vídeo, gif ou {sticker|figurinha}{| aí}, ou {respond{a|e} |marca }{|{a|uma} mensagem {com |que seja }}{{alguma|uma} mídia|{algum|um} arquivo} {pra|para} eu {transformar em|criar uma} figurinha{.|!} {|Pq se não eu não vou conseguir {tirar do {cu|rabo} oq vc quer{|!| criar| fazer}{|!| kk| uai}|fazer nada{|!| né?| uai| kk}}|Pq eu não tenho bola de cristal{!| kk|porra!}}',
+    '{|{Pô|Poxa|Porra|Se liga} {véi|mano|bro|brother|bixo|bicho}{| kk}{.|!|} }{Manda|Envia|Me manda|Me envia} uma {foto|imagem}, vídeo, gif ou {sticker|figurinha}{| aí}, ou {respond{a|e} |marca }{|{a|uma} mensagem {com |que seja }}{{alguma|uma} mídia|{algum|um} arquivo} {pra|para} eu {transformar em|criar uma} figurinha{.|!}{|{ Pq {se não|senão} eu {não vou conseguir|vou ter que} {tirar do {cu|rabo} oq vc quer{|!| criar| fazer}{|!| kk| uai}|fazer {nada|porra nenhuma}{|!|, né?| uai| kk}}| Pq eu não tenho bola de cristal{!| kk| porra!}|{ Senão| Se não} eu não consigo fazer porra nenhuma{|, né?| uai| kk|!|!!!}}}',
 
   creationFailed:
-    '{|{Eita|Vixe|Vish|Uai}{!|!!|!!!} }{Não consegui|Deu ruim|Falhei bonito} {ao|de}{| tentar} criar a figurinha{.|!|!!|!!!}\n{Tenta de novo|Tente novamente|Joga outra aí|Manda {novamente|de novo}}, vai que{| agora} {{|o {bagulho|esse trem}} resolve funcionar|da certo?}{| kk| uai| né?}',
+    '{|{Eita|Vixe|Vish|Uai|Porra{| caralho}|Caralho|Putz}{!|!!|!!!} }{Não consegui|Deu ruim{| ao}{| tentar}|Falhei{| bonito| miseravelmente}{| tentando}} criar a figurinha{.|!|!!|!!!}\n{Tenta de novo|Tente novamente|Joga outra aí|Manda {novamente|de novo}}, vai que{| agora} {{|o {bagulho|esse trem}} resolve funcionar|da certo?}{| kk| uai| né?}',
 
   stealMissingMedia:
-    '{|{Pô|Poxa|Porra|Se liga} {véi|mano|bro|brother|bixo|bicho}{|.|!| kk} }{{Vc|Você|Ocê} tem que {responde|reponder|macar|marca}|{Responda|Responde|Marca}} {a|uma|a uma}{ | mensagem {que tenha|com uma|da}} figurinha{| aí} pra eu {trocar o autor/pacote|roubar|trocar os metadados|roubar pra vc}{|.|!|}{| uai| kk}',
+    '{|{Pô|Poxa|Porra|Se liga} {véi|mano|bro|brother|bixo|bicho}{|.|!| kk} }{{Vc|Você|Ocê} tem que {responde|reponder|macar|marca}|{Responda|Responde|Marca}} {a|uma|a uma}{ | mensagem {que tenha|com uma|da}} figurinha{| aí} pra eu {|conseguir }{trocar o autor/pacote|roubar|trocar os metadados|roubar {pra vc|ela}}{|.|!|}{| uai| kk}',
 
   toMediaMissing:
     '{|{Pô|Poxa|Porra|Se liga} {véi|mano|bro|brother|bixo|bicho}{| kk}{|.|!|} }{{Vc|Você|Ocê} tem que {responde|reponder|macar|marca}|{Responda|Responde|Marca}} {a|uma|a uma}{ | mensagem {que tenha|com uma|da}} figurinha{| aí} {pra|para} eu {|conseguir }converter em {{imagem|foto} ou vídeo|arquivo}{.|!|!!!}{| Senão {vc|você|ocê} me complica| Ai fica dificil| Fica dificil assim}{| kk| uai}',
@@ -19,5 +19,5 @@ export const stickerMessages = {
   },
 
   conversionFailed:
-    '{|{Opa|Oops|Eita}{!} }{Não consegui|Deu ruim ao|Falhei ao|Deu xablau tentando} {converter|criar|fazer} a figurinha. {{Tenta|Manda} {outra mídia|outro arquivo}|{Tenta|Tente} de novo}{| aí| ai}{.|!|!!|!!!| uai| kk}'
+    '{|{Opa|Oops|Eita|Putz}{!} }{Não consegui|Deu ruim ao|Falhei ao|Deu xablau tentando} {converter|criar|fazer} a figurinha. {{Tenta|Manda} {outra mídia|outro arquivo}|{Tenta|Tente} de novo}{| aí| ai}{.|!|!!|!!!| uai| kk}'
 }
